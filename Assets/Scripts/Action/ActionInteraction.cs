531d083315cadec1b69bdb56f95d88c4ca9faeb8
Unknown <gsiliutop@gmail.com>
2019-09-18 22:28:51 +0200

    InteractuarObjetos
    Meter el interactuar, animacion y voz

---
+++ Assets/Scripts/Action/ActionInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionInteraction : MonoBehaviour
+{
+    public Animator anim;
+    public AudioClip[] actionSnd;
+    public AudioSource audioS;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            anim.gameObject.SetActive(false);
+            anim.gameObject.SetActive(true);
+            audioS.PlayOneShot(actionSnd[Random.Range(0,actionSnd.Length)]);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            anim.SetBool("canContinue", true);
+        
+    }
+}
---
+++ Assets/Scripts/Utils/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Utils : MonoBehaviour
+{
+    //Pasar la animacion por parametro, cuando esta haya finalizado devolverá true
+    public static bool AnimationIsFinished(Animator anim)
+    {
+        if (anim.isActiveAndEnabled)
+        {
+            if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+                return true;
+            else
+                return false;
+        }
+        else
+            return false;
+
+    }
+}
