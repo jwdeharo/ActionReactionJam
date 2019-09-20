189b83ae7aa3c5081fd3d578153d03199cfdae9f
James <jwdeharo@gmail.com>
2019-09-21 00:17:15 +0200

    Implementing the fsm for the killer frankie

---
+++ Assets/Scripts/FrankieTheKIller/States/FrankieTheKillerIdleState.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FrankieTheKillerIdleState : CState
+{
+    /**
+     * Function that is called while the state is the current.
+     */
+    public override void UpdateState()
+    {
+        Debug.Log("holi caracoli");
+    }
+}
--- Assets/Scripts/FrankieTheKIller/ViewFinderController.cs
+++ Assets/Scripts/FrankieTheKIller/ViewFinderController.cs
@@ -1,21 +1,43 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class ViewFinderController : BaseController
 {
+    private bool FirstTimeTrigger;
+    private SpriteRenderer MySpriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        //MyFSM = GetComponent<FSM>();
-        //MyFSM.StartFSM();
+        FrankieTheKillerIdleState IdleState = new FrankieTheKillerIdleState();
+        IdleState.Init(this);
+
+        States = new Dictionary<string, CState>();
+        States.Add("Idle", IdleState);
+
+        MyFSM = GetComponent<FSM>();
+        MyFSM.StartFSM();
+
+        FirstTimeTrigger = true;
+        MySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void StartChasing()
     {
-        Debug.Log("Starting the chase");
+        if (FirstTimeTrigger)
+        {
+            MySpriteRenderer.enabled = true;
+            FirstTimeTrigger = false;
+
+            MyFSM.SetActive(true);
+            MyFSM.ChangeState(States["Idle"]);
+        }
     }
 
     private void StopChasing()
     {
-        Debug.Log("Stoping the chase");
+        MySpriteRenderer.enabled = false;
+        FirstTimeTrigger = true;
+        MyFSM.SetActive(false);
     }
 }
