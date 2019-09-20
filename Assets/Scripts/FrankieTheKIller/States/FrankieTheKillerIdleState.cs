fd4954db26a437608d2f93489fb400a91874a15f
James <jwdeharo@gmail.com>
2019-09-21 00:44:48 +0200

    In the idle state, frankie the killer will move in circles

--- Assets/Scripts/FrankieTheKIller/States/FrankieTheKillerIdleState.cs
+++ Assets/Scripts/FrankieTheKIller/States/FrankieTheKillerIdleState.cs
@@ -2,11 +2,31 @@
 
 public class FrankieTheKillerIdleState : CState
 {
+    private float RotationRadius = 0.003f;
+    private float AngularSpeed = 2.0f;
+    private float PositionX;
+    private float PositionY;
+    private float Angle;
+
+    public override void OnEnterState()
+    {
+        Angle = 0.0f;
+    }
+
     /**
      * Function that is called while the state is the current.
      */
     public override void UpdateState()
     {
-        Debug.Log("holi caracoli");
+        PositionX = Controller.transform.position.x + Mathf.Cos(Angle) * RotationRadius;
+        PositionY = Controller.transform.position.y + Mathf.Sin(Angle) * RotationRadius;
+
+        Controller.transform.position = new Vector3(PositionX, PositionY, 0.0f);
+        Angle += Time.deltaTime * AngularSpeed;
+
+        if (Angle > 360.0f)
+        {
+            Angle = 0.0f;
+        }
     }
 }
