7c4d4f1b6fefa2b437974d95ed5263488510a590
samusfc <samgonzul@hotmail.com>
2019-09-22 01:18:00 +0200

    Add files via upload

---
+++ Assets/Scripts/Start/StartPlay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartPlay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            print("Has pulsado escape");
+            SceneManager.LoadScene("Juego");
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Has pulsado escape");
+            Application.Quit();
+        }
+    }
+}
