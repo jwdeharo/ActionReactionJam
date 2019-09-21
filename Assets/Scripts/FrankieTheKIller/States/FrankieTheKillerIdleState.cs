27ea31249f3b53e1d68a6510dd634124aa9103aa
James <jwdeharo@gmail.com>
2019-09-21 12:41:18 +0200

    Some changes in the structure

--- Assets/Scripts/FrankieTheKIller/States/FrankieTheKillerIdleState.cs
+++ Assets/Scripts/FrankieTheKIller/States/FrankieTheKillerIdleState.cs
@@ -2,15 +2,12 @@
 
 public class FrankieTheKillerIdleState : CState
 {
-    private float RotationRadius = 0.003f;
-    private float AngularSpeed = 2.0f;
-    private float PositionX;
-    private float PositionY;
-    private float Angle;
+    private ViewFinderController MyFrankieController;
 
-    public override void OnEnterState()
+    public override void Init(BaseController aParent)
     {
-        Angle = 0.0f;
+        base.Init(aParent);
+        MyFrankieController = (ViewFinderController)Controller;
     }
 
     /**
@@ -18,15 +15,6 @@ public class FrankieTheKillerIdleState : CState
      */
     public override void UpdateState()
     {
-        PositionX = Controller.transform.position.x + Mathf.Cos(Angle) * RotationRadius;
-        PositionY = Controller.transform.position.y + Mathf.Sin(Angle) * RotationRadius;
-
-        Controller.transform.position = new Vector3(PositionX, PositionY, 0.0f);
-        Angle += Time.deltaTime * AngularSpeed;
-
-        if (Angle > 360.0f)
-        {
-            Angle = 0.0f;
-        }
+        MyFrankieController.CircleMovement();
     }
 }
--- Assets/Scripts/FrankieTheKIller/ViewFinderController.cs
+++ Assets/Scripts/FrankieTheKIller/ViewFinderController.cs
@@ -3,8 +3,18 @@ using UnityEngine;
 
 public class ViewFinderController : BaseController
 {
-    private bool FirstTimeTrigger;
-    private SpriteRenderer MySpriteRenderer;
+    private bool            FirstTimeTrigger;
+    private float Angle = 0.0f;
+
+    private SpriteRenderer  MySpriteRenderer;
+    private BoxCollider2D   MyCollider;
+
+    [SerializeField]
+    private GameObject      Target;
+    [SerializeField]
+    private float AngularSpeed = 1.0f;
+    [SerializeField]
+    private float RotationRadius = 1.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +30,7 @@ public class ViewFinderController : BaseController
 
         FirstTimeTrigger = true;
         MySpriteRenderer = GetComponent<SpriteRenderer>();
+        MyCollider = GetComponent<BoxCollider2D>();
     }
 
     private void StartChasing()
@@ -27,6 +38,7 @@ public class ViewFinderController : BaseController
         if (FirstTimeTrigger)
         {
             MySpriteRenderer.enabled = true;
+            MyCollider.enabled = true;
             FirstTimeTrigger = false;
 
             MyFSM.SetActive(true);
@@ -37,7 +49,36 @@ public class ViewFinderController : BaseController
     private void StopChasing()
     {
         MySpriteRenderer.enabled = false;
+        MyCollider.enabled = false;
+
         FirstTimeTrigger = true;
         MyFSM.SetActive(false);
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D aCollision)
+    {
+        if (aCollision.gameObject == Target)
+        {
+            Target.SendMessage("ChangeToDeath");
+        }
+    }
+
+    private float GetPosition(float aPosition, float aMathFAngle)
+    {
+        return aPosition + (aMathFAngle * RotationRadius);
+    }
+
+    public void CircleMovement()
+    {
+        transform.position = new Vector3(GetPosition(transform.position.x, Mathf.Cos(Angle)), 
+            GetPosition(transform.position.y, Mathf.Sin(Angle)), 0.0f);
+
+        Angle += Time.deltaTime * AngularSpeed;
+
+        if (Angle > 360.0f)
+        {
+            Angle = 0.0f;
+        }
     }
 }
