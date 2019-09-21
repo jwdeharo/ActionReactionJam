12b00a5799c92aa2164af254a90cafef52f4d911
James <jwdeharo@gmail.com>
2019-09-21 20:17:13 +0200

    Simple is better

--- Assets/Scripts/Player/States/HidingState.cs
+++ Assets/Scripts/Player/States/HidingState.cs
@@ -4,14 +4,8 @@ public class HidingState : CState
 {
     public override void UpdateState()
     {
-        ((PlayerController)Controller).Animator.SetFloat("Speed", 1.0f);
-        Controller.transform.position = Vector2.MoveTowards(Controller.transform.position, ((PlayerController)Controller).GetToHideTransform().position, 0.5f * Time.deltaTime);
-        if (Vector2.Distance(Controller.transform.position, ((PlayerController)Controller).GetToHideTransform().position) == 0.0f)
-        {
-            
-            ((PlayerController)Controller).Animator.SetFloat("Speed", -1.0f);
-            ((PlayerController)Controller).ChangeSprite(false);
-            Controller.GetFSM().PopState();
-        }
+        ((PlayerController)Controller).Animator.SetFloat("Speed", -1.0f);
+        ((PlayerController)Controller).ChangeSprite(false);
+        Controller.GetFSM().PopState();
     }
 }
