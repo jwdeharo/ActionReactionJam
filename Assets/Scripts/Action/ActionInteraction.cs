4f9c1185812f73f34f115f7b5c7ae52f36584484
James <jwdeharo@gmail.com>
2019-09-22 01:52:50 +0200

    action reaction baby

--- Assets/Scripts/Action/ActionInteraction.cs
+++ Assets/Scripts/Action/ActionInteraction.cs
@@ -20,6 +20,7 @@ public class ActionInteraction : MonoBehaviour
 
     public Animator anim;
     public AudioClip[] actionSnd;
+    public AudioClip[] reactionSnd;
     public AudioSource audioS;
     public GameObject[] choices;
     public DeployChoices deployChoicesScript;
@@ -115,6 +116,8 @@ public class ActionInteraction : MonoBehaviour
 
     private void DoAction<T>(T aToSend, bool aSendParam)
     {
+        audioS.PlayOneShot(reactionSnd[Random.Range(0, reactionSnd.Length)]);
+
         if (aSendParam)
         {
             OnlyChoice.SendMessage(ActionToDo, aToSend);
--- Assets/Scripts/Player/States/DeathState.cs
+++ Assets/Scripts/Player/States/DeathState.cs
@@ -1,12 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DeathState : CState
+﻿public class DeathState : CState
 {
     public override void OnEnterState()
     {
-        Debug.Log("You died");
-        //((PlayerController)Controller).Animator.SetBool("IsDead", true);
+        ((PlayerController)Controller).Animator.SetBool("IsDead", true);
     }
 }
