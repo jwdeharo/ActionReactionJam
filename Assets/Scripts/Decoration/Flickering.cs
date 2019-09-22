bf1d31af95444c45881f04b33cd31e02e85384ce
Unknown <gsiliutop@gmail.com>
2019-09-22 15:21:58 +0200

    No hay llaves en coche

---
+++ Assets/Scripts/Decoration/Flickering.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flickering : MonoBehaviour
+{
+    SpriteRenderer spriteR;
+    WaitForSeconds wait;
+    private void Start()
+    {
+        spriteR = GetComponent<SpriteRenderer>();
+        wait = new WaitForSeconds(0.5f);
+    }
+
+    public IEnumerator Flick()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            print("flik");
+            yield return wait;
+            spriteR.enabled=true;
+            yield return wait;
+            spriteR.enabled = false;
+            print("flok");
+        }
+        
+    }
+
+}
