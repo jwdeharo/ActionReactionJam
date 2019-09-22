ed5e130575a8dd3e5f2dc4cdcbdb17dea352e6c8
Unknown <gsiliutop@gmail.com>
2019-09-22 12:46:21 +0200

    Awita bebe la liadinha

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,11 +7,8 @@ public class PlayerController : BaseController
     public float MoveSpeed = 5.0f;                  //!< Velocity of the player.
     public bool FirekeepersSeen;
     public Animator Animator;                       //!< Animator which handles the animations.
-    public bool FirekeepersSeen;
-
     private Sprite OriginalSprite;
     private Transform ToHideTransform;
-
 
     [SerializeField]
     private GameObject HidingSprite = null;
@@ -63,7 +60,6 @@ public class PlayerController : BaseController
         BoxToPlayer = false;
 
         OriginalSprite = GetComponent<SpriteRenderer>().sprite;
-        Animator = GetComponent<Animator>();
     }
 
     /**
@@ -106,8 +102,6 @@ public class PlayerController : BaseController
     {
         if (IsTypeObject<MovableObjectsController>(aCollision) && GetMovement().x != 0.0f)
         {
-            print("empujar");
-            Animator.SetBool("isPushing", true);
             MovingSpeedFactor = 0.1f;
             aCollision.gameObject.SendMessage("ApplyMovement", GetMovement());
         }
@@ -121,7 +115,6 @@ public class PlayerController : BaseController
     {
         if (IsTypeObject<MovableObjectsController>(aCollision))
         {
-            Animator.SetBool("isPushing", false);
             aCollision.gameObject.SendMessage("ApplyMovement", Vector3.zero);
             MovingSpeedFactor = 1.0f;
         }
