436e9b1ae3bf59b11941171b102810fa2093ac81
samusfc <samgonzul@hotmail.com>
2019-09-22 03:53:59 +0200

    Add files via upload

---
+++ Assets/Scripts/Firefighters/Firefighters.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firefighters : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Target = null;
+    //public PlayerController player;
+
+    public void OnTriggerEnter2D(Collider2D aCollision)
+    {
+        if(aCollision.gameObject == Target)
+        {
+           aCollision.gameObject.GetComponent<PlayerController>().FirekeepersSeen = true;
+           print(aCollision.gameObject.GetComponent<PlayerController>().FirekeepersSeen);
+        }
+    }
+}
