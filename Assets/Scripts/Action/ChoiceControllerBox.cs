0560a48eaf6770e92b7c6b889dcf3d45e7f5b71e
Unknown <gsiliutop@gmail.com>
2019-09-22 14:19:57 +0200

    La caja que sube y baja ataca de nuevo

--- Assets/Scripts/Action/ChoiceControllerBox.cs
+++ Assets/Scripts/Action/ChoiceControllerBox.cs
@@ -2,12 +2,21 @@
 
 public class ChoiceControllerBox : MonoBehaviour
 {
-    public BoxSwitchManager boxSwitchScript;
+   // public BoxSwitchManager boxSwitchScript;
+    public Rigidbody2D playerBody;
 
     public void PerformAction()
     {
-        boxSwitchScript.InitializePositions();
+        //boxSwitchScript.InitializePositions();
         GetComponent<BoxCollider2D>().enabled = true;
         transform.position = new Vector2(transform.position.x, -0.57f);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            playerBody.constraints = RigidbodyConstraints2D.None;
+        }
     }
 }
